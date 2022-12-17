8a83d505f6425c2233dcb8bc05563f3b5a44c752
Take434 <timh4376@gmail.com>
2022-12-17 13:23:49 +0100

    found out how to use the fast-png library

--- waveFunctionAlgorithm.js
+++ waveFunctionAlgorithm.js
@@ -24,22 +24,51 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
+const png = __importStar(require("fast-png"));
+const iobuffer_1 = require("iobuffer");
 class WfcModel {
-    constructor(path) {
+    constructor(path, outputDims) {
         var jsonData = JSON.parse(fs.readFileSync('' + path, 'utf-8'));
-        console.log(jsonData);
         this.lookUp = jsonData.map(e => e.Path);
         this.weightings = jsonData.map(e => e.Weight);
         this.ruleset = jsonData.map(e => e.Rules);
-        console.log("and now my stuff");
-        console.log(this.lookUp);
-        console.log("---------------------------");
-        console.log(this.ruleset);
-        console.log("---------------------------");
-        console.log(this.weightings);
+        this.output = new Array(outputDims[0]);
+        for (let i = 0; i < outputDims[0]; i++) {
+            this.output[i] = new Array(outputDims[1]);
+            for (let j = 0; j < outputDims[1]; j++) {
+                this.output[i][j] = new Array(this.lookUp.length);
+                this.output[i][j] = this.output[i][j].fill(true);
+            }
+        }
+        this.entropyMap = new Array(outputDims[0]);
+        for (let i = 0; i < outputDims[0]; i++) {
+            this.entropyMap[i] = new Array(outputDims[1]);
+            this.entropyMap[i] = this.entropyMap[i].fill(this.lookUp.length);
+        }
     }
-    wfc(width, height) {
-        return [[[]]];
+    collapse() {
+    }
+    // public getResultAsDecodedPNG() : DecodedPng {
+    // }
+    saveResultAsFile(filePath) {
+        var img = fs.createReadStream('tilesets/test.png');
+        var imgBuff;
+        let chunks = [];
+        img.on('data', (chunk) => {
+            chunks.push(chunk);
+        });
+        img.once('end', () => {
+            imgBuff = new iobuffer_1.IOBuffer(Buffer.concat(chunks));
+            var a = png.decode(imgBuff);
+            a.data[0] = 3;
+            a.data[1] = 36;
+            a.data[2] = 252;
+            var b = png.encode(a);
+            fs.writeFile('' + filePath, b, () => {
+                console.log("all written up boss");
+            });
+        });
     }
 }
-let a = new WfcModel('tilesets/tileset1/rules.json');
+let a = new WfcModel('tilesets/tileset1/rules.json', [5, 5]);
+a.saveResultAsFile("testChanges.png");
--- waveFunctionAlgorithm.ts
+++ waveFunctionAlgorithm.ts
@@ -1,4 +1,7 @@
 import * as fs from 'fs';
+import * as png from 'fast-png';
+import { IOBuffer } from 'iobuffer';
+import { DecodedPng } from 'fast-png';
 
 type jsonRules = [
     {
@@ -14,27 +17,76 @@ class WfcModel {
     private weightings : number[];
     private ruleset : number[][][];
 
-    constructor(path : String) {
-        var jsonData : jsonRules = JSON.parse(fs.readFileSync('' + path, 'utf-8'));
+    private output : boolean[][][];
+    private entropyMap : number[][];
 
-        console.log(jsonData);
+    constructor(path : String, outputDims : [number, number]) {
+        var jsonData : jsonRules = JSON.parse(fs.readFileSync('' + path, 'utf-8'));
 
         this.lookUp = jsonData.map(e => e.Path);
         this.weightings = jsonData.map(e => e.Weight);
         this.ruleset = jsonData.map(e => e.Rules);
 
-        console.log("and now my stuff");
+        this.output = new Array(outputDims[0]);
+        for(let i = 0; i < outputDims[0]; i++) {
+            this.output[i] = new Array(outputDims[1]);
+            
+            for(let j = 0; j < outputDims[1]; j++) {
+                this.output[i][j] = new Array(this.lookUp.length);
+                this.output[i][j] = this.output[i][j].fill(true);
+            }
+        }
 
-        console.log(this.lookUp)
-        console.log("---------------------------");
-        console.log(this.ruleset);
-        console.log("---------------------------");
-        console.log(this.weightings);
+        this.entropyMap = new Array(outputDims[0]);
+        for(let i = 0; i < outputDims[0]; i++) {
+            this.entropyMap[i] = new Array(outputDims[1]);
+            this.entropyMap[i] = this.entropyMap[i].fill(this.lookUp.length);
+        }
     }
 
-    public wfc(width : number, height : number) : boolean[][][]{
-        return [[[]]];
+    public collapse() {
+        
     }
+
+    // public getResultAsDecodedPNG() : DecodedPng {
+        
+    // }
+
+    public saveResultAsFile(filePath : string) {
+        var img = fs.createReadStream('tilesets/test.png');
+        var imgBuff : IOBuffer;
+        let chunks : Buffer[] = [];
+
+        img.on('data', (chunk) => {
+            chunks.push(chunk as Buffer);
+        });
+
+        img.once('end', () => {
+            imgBuff = new IOBuffer(Buffer.concat(chunks));
+            var a = png.decode(imgBuff);
+
+            a.data[0] = 3;
+            a.data[1] = 36;
+            a.data[2] = 252;
+
+            var b = png.encode(a);
+
+            fs.writeFile('' + filePath, b, () => { 
+                console.log("all written up boss");
+            });
+        });
+    }
+
+    // private chooseNextField() : [number, number] {
+
+    // }
+
+    // private chooseTileToPlace() : number {
+
+    // }
+
+    // private propagateChanges() {}
 }
 
-let a = new WfcModel('tilesets/tileset1/rules.json');
+let a = new WfcModel('tilesets/tileset1/rules.json', [5, 5]);
+a.saveResultAsFile("testChanges.png");
